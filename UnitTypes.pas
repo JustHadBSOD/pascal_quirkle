3b217bd833aa87edb38caf190daff2e5bc8aa64d
JustHadBSOD <39551750+JustHadBSOD@users.noreply.github.com>
2018-05-23 12:12:21 +0200

    Add files via upload

--- UnitTypes.pas
+++ UnitTypes.pas
@@ -3,10 +3,15 @@ Unit UnitTypes;
 interface
 
 
+TYPE
+	form = Array [0..7] of Char;
+
+CONST 
+	tableauforme : form = ('a','b','c','d','e','f','g','h');
 
 TYPE tuile = Record
-	couleur = Integer;
-	forme = Array ['','','','','','','',''];
+	couleur : Integer;
+	forme : Integer;
 END;
 
 TYPE
@@ -22,27 +27,40 @@ TYPE
 	inventory = ^maillon;
 
 TYPE
-	Grille = Array of tuile;
+	Grille = Array of Array of tuile;
 
 TYPE player = Record
-	nom = string;
-	points = Integer;
-	main = inventory;
-	humain = Boolean;
-	age = Integer;
+	nom : string;
+	points : Integer;
+	main : inventory;
+	humain : Boolean;
+	age : Integer;
+END;
+
+
+TYPE
+	maillonfaible = Record
+	data : player;
+	Address : ^maillon;
 END;
 
 TYPE
-	listejoueurs = ^maillon;
+	listejoueurs = ^maillonfaible;
 
 TYPE parametres_de_jeu = Record
-	nb_tuiles_identiques = Integer;
-	nb_couleurs = Integer;
-	nb_formes = Integer;
+	nb_tuiles_identiques : Integer;
+	nb_couleurs : Integer;
+	nb_formes : Integer;
+END;
 
 TYPE jeu = Record
-	tilebag = pioche;
-	plateau = Grille;
-	playerlist = listejoueurs;
-	gameparameters = parametres_de_jeu;
+	tilebag : pioche;
+	plateau : Grille;
+	playerlist : listejoueurs;
+	gameparameters : parametres_de_jeu;
 END;
+
+
+implementation
+
+END.
